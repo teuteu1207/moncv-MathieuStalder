91e4c702dd93be42ff5647368b6d6279694ef97e
teuteu1207 <mathieu.stalder@gmail.com>
2018-11-01 11:34:26 +0100

    update SiteWeb

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import $ from "jquery";
 window.$ = $;
 import "bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
+import "bootswatch/dist/sandstone/bootstrap.min.css";
 import "./main.css";
 
 $(document).ready(() => {
